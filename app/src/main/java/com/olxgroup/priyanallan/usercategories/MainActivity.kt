5fd0868d38f3e30ec7c6bb35729f7310d015c8d5
Vishnu Priya Nallan <nallan@karlmax-berlin.com>
2019-06-08 10:56:36 +0200

    Create navigation graph prototype

--- app/src/main/java/com/olxgroup/priyanallan/usercategories/MainActivity.kt
+++ app/src/main/java/com/olxgroup/priyanallan/usercategories/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.olxgroup.priyanallan.usercategories
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +9,4 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
 }
