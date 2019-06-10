f433ab4ed1b12ec68aa3f6ecb7d88846c2392418
Vishnu Priya Nallan <nallan@karlmax-berlin.com>
2019-06-10 17:56:09 +0200

    Create UIs for Category screen and category item detail screen

--- app/src/main/java/com/olxgroup/priyanallan/usercategories/ui/CatgoryItemsFragment.kt
+++ app/src/main/java/com/olxgroup/priyanallan/usercategories/ui/CatgoryItemsFragment.kt
@@ -8,12 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.olxgroup.priyanallan.usercategories.R
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  *
@@ -27,6 +21,5 @@ class CatgoryItemsFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_category_items, container, false)
     }
-
 
 }
---
+++ app/src/main/java/com/olxgroup/priyanallan/usercategories/ui/DisplayFragment.kt
@@ -0,0 +1,12 @@
+package com.olxgroup.priyanallan.usercategories.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+open class DisplayFragment : Fragment() {
+
+
+}
